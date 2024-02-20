da3ea2cbea8eeea722f790386ff84dd0aadfa99a
trygvebechsgaard <trygvemb@gmail.com>
2024-02-20 10:52:57 +0545

    updated gitignore to ignore all docker files

--- Rally/Program.cs
+++ Rally/Program.cs
@@ -33,7 +33,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<RallyDbContext>();
-    context.Database.EnsureCreated();
+   // context.Database.EnsureCreated();
     // DbInitializer.Initialize(context);
 }
 
