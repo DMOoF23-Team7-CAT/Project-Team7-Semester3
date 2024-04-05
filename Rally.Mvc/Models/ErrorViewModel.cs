a69f12231862d994719d16c6612d772bd965a2ec
trygvebechsgaard <trygvemb@gmail.com>
2024-04-05 11:02:49 +0700

    Initial commit

--- Rally.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Rally.Mvc.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ src/Rally.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Rally.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
