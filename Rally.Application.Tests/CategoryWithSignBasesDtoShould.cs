1e23eb780169c2ab340003d91187465e760496d8
clan54056 <128791046+clan54056@users.noreply.github.com>
2024-04-29 13:11:00 +0200

    Gem

---
+++ Rally.Application.Tests/CategoryWithSignBasesDtoShould.cs
@@ -0,0 +1,40 @@
+﻿using Rally.Application.Dto.Category;
+
+namespace Rally.Application;
+
+public class CategoryWithSignBasesDtoShould
+{
+    [Fact]
+    public void HaveNameInCorrectFormat()
+    {
+        CategoryWithSignBasesDto sut = new CategoryWithSignBasesDto();
+
+        sut.Name = "Begynder bane";
+
+        Assert.Matches("^[A-Za-zæøåÆØÅ ]+$", sut.Name);
+    }
+
+    [Fact]
+    public void NotHaveNameByDefault()
+    {
+        CategoryWithSignBasesDto sut = new CategoryWithSignBasesDto();
+
+        Assert.Empty(sut.Name);
+    }
+
+    [Fact]
+    public void NotHaveRulesByDefault()
+    {
+        CategoryWithSignBasesDto sut = new CategoryWithSignBasesDto();
+
+        Assert.Empty(sut.Rules);
+    }
+
+    [Fact]
+    public void HaveIdByDefault()
+    {
+        CategoryWithSignBasesDto sut = new CategoryWithSignBasesDto();
+
+        Assert.NotNull(sut.Id);
+    }
+}
