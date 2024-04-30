2ef1d8672700f0f168f335f9d4c66581303bad05
Adam Eleraky <adam_eleraky@hotmail.com>
2024-04-30 21:17:31 +0200

    Unit test for Infrastructure implemented with In Memory database and fluent assertions

---
+++ Infrastructure.Test/RallyInfrastructureTest.cs
@@ -0,0 +1,39 @@
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Rally.Core.Entities;
+using Rally.Infrastructure.Data;
+using Rally.Infrastructure.Repositories;
+using Rally.Infrastructure.Repositories.Base;
+
+namespace Infrastructure.Test
+{
+    public class RallyInfrastructureTest
+    {
+        [Fact]
+        public async Task CategoryRepository_GetAllAsync_ShouldReturnAllCategories()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<RallyContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase") // Use in-memory database
+                .Options;
+
+            using var context = new RallyContext(options);
+
+            context.Categories.Add(new Category { Id = 1, Name = "Category1" });
+            context.Categories.Add(new Category { Id = 2, Name = "Category2" });
+            context.Categories.Add(new Category { Id = 3, Name = "Category3" });
+            await context.SaveChangesAsync();
+
+            var repository = new CategoryRepository(context);
+
+            // Act
+            var categories = await repository.GetAllAsync();
+
+            // Assert
+            categories.Should().BeOfType<List<Category>>();
+            categories.Should().NotBeNull();
+            categories.Should().HaveCount(3);
+            categories.First().Name.Should().Be("Category1");
+        }
+    }
+}
