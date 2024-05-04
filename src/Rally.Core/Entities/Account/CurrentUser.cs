b1d39ae81e76b8191cd0bdd2e2e8573a93c157f0
Adam Eleraky <adam_eleraky@hotmail.com>
2024-05-04 12:29:29 +0200

    CreateTrackWithUser endpoint works and track is added to user in DB. Mapping needs fix

--- src/Rally.Core/Entities/Account/CurrentUser.cs
+++ src/Rally.Core/Entities/Account/CurrentUser.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Rally.Core.Entities.Account
 {
-    public class CurrentUser(string Id, string Email, IEnumerable<string> Roles)
+    public record CurrentUser(string Id, string Email, IEnumerable<string> Roles)
     {
         public bool IsInRole(string role)
         {
