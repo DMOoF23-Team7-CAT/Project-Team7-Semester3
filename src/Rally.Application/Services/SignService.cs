713f8a7669637efee8ba786b78994a78622535d3
Adam Eleraky <adam_eleraky@hotmail.com>
2024-05-08 12:33:37 +0200

    SignService implemented with related SignValidator class

--- src/Rally.Application/Services/SignService.cs
+++ src/Rally.Application/Services/SignService.cs
@@ -1,6 +1,10 @@
+using FluentValidation;
 using Rally.Application.Dto.Sign;
+using Rally.Application.Dto.SignBase;
+using Rally.Application.Exceptions;
 using Rally.Application.Interfaces;
 using Rally.Application.Mapper;
+using Rally.Application.Validators;
 using Rally.Core.Entities;
 using Rally.Core.Repositories;
 
@@ -14,29 +18,77 @@ namespace Rally.Application.Services
             _signRepository = signRepository ?? throw new ArgumentNullException(nameof(signRepository));
         }
 
-        public Task<IEnumerable<SignDto>> GetAll()
+        public async Task<IEnumerable<SignDto>> GetAll()
         {
-            throw new NotImplementedException();
+            var signs = await _signRepository.GetAllAsync();
+            if (signs is null)
+                throw new NotFoundException("Signs could not be found.");
+
+            var mappedSigns = ObjectMapper.Mapper.Map<IEnumerable<SignDto>>(signs);
+            if (mappedSigns is null)
+                throw new MappingException("Signs could not be mapped.");
+
+            return mappedSigns;
         }
 
-        public Task<SignDto> GetById(int id)
+        public async Task<SignDto> GetById(int id)
         {
-            throw new NotImplementedException();
+            var sign = await _signRepository.GetByIdAsync(id);
+            if (sign is null)
+                throw new NotFoundException("Sign could not be found.");
+
+            var mappedSign = ObjectMapper.Mapper.Map<SignDto>(sign);
+            if (mappedSign is null)
+                throw new MappingException("Sign could not be mapped.");
+
+            return mappedSign;
         }
 
-        public Task<SignDto> Create(SignWithoutIdDto dto)
+        public async Task<SignDto> Create(SignWithoutIdDto dto)
         {
-            throw new NotImplementedException();
+            var sign = ObjectMapper.Mapper.Map<Sign>(dto);
+            if (sign is null)
+                throw new MappingException("Sign could not be mapped.");
+
+            var validator = new SignValidator();
+            var validationResult = await validator.ValidateAsync(sign);
+            if (!validationResult.IsValid)
+                throw new ValidationException(validationResult.Errors);
+
+            await _signRepository.AddAsync(sign);
+
+            var newSign = ObjectMapper.Mapper.Map<SignDto>(sign);
+            if(newSign is null)
+                throw new MappingException("Sign could not be mapped.");
+
+            return newSign;
         }
 
-        public Task Delete(int id)
+        public async Task Delete(int id)
         {
-            throw new NotImplementedException();
+            var sign = await _signRepository.GetByIdAsync(id);
+            if (sign is null)
+                throw new NotFoundException("Sign could not be found.");
+
+            await _signRepository.DeleteAsync(sign);
         }
 
-        public Task Update(SignDto dto)
+        public async Task Update(SignDto dto)
         {
-            throw new NotImplementedException();
+            var oldSign = await _signRepository.GetByIdAsync(dto.Id);
+            if (oldSign is null)
+                throw new NotFoundException("Sign could not be found.");
+
+            var sign = ObjectMapper.Mapper.Map<Sign>(dto);
+            if (sign is null)
+                throw new MappingException("Sign could not be mapped.");
+
+            var validator = new SignValidator();
+            var validationResult = await validator.ValidateAsync(sign);
+            if (!validationResult.IsValid)
+                throw new ValidationException(validationResult.Errors);
+
+            await _signRepository.UpdateAsync(ObjectMapper.Mapper.Map(dto, oldSign));
         }
 
         public async Task<SignWithSignBaseDto> GetSignWithSignBases(int signId)
@@ -45,7 +97,7 @@ namespace Rally.Application.Services
 
             var mappedSign = ObjectMapper.Mapper.Map<SignWithSignBaseDto>(sign);
             if (mappedSign is null)
-                throw new ApplicationException("Sign with SignBases could not be mapped");
+                throw new MappingException("Sign with SignBases could not be mapped");
 
             return mappedSign;
         }
